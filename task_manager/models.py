72471a0ce9b88437a407477cbc02cdacc586b023
kostya-kononenko <kkononenko3@gmail.com>
2023-06-13 12:41:22 +0300

    Add Admin models: PositionAdmin, WorkerAdmin, TaskTypeAdmin, TaskAdmin

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
@@ -22,6 +25,9 @@ class Worker(AbstractUser):
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
