e0753e26ff4fadac77db94588eb7df8ee8210bc4
kostya-kononenko <kkononenko3@gmail.com>
2023-06-13 11:43:04 +0300

    Add models: Position, Worker, TaskType, Task and make migrations

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        low = "low", "low"
+        medium = "medium", "medium"
+        high = "high", "high"
+        very_high = "very high", "very high"
+
+    name = models.CharField(max_length=255)
+    descriptions = models.CharField(max_length=255)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=9, choices=Priority.choices, default=Priority.low)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="workers")
+
+    def __str__(self):
+        return f"{self.name} {self.descriptions} {self.deadline}"
+
+    class Meta:
+        ordering = ["name"]
