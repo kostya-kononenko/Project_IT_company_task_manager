9d93120796c497bc940349e1f28ffc8d4d9c066b
kostya-kononenko <kkononenko3@gmail.com>
2023-06-20 13:56:57 +0300

    move SECRET_KEY to env file. with python-dotenv

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from task_manager import context_processors
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    "django-insecure-6dcb@u%f0^lm2m3bh8dza#q1r+^nig!8o5r^nk23wrwlhxm2t3"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
